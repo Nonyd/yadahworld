ea852e946f37563da66edf96a8612e45c40628e5
Nonyd <itsnonyd@gmail.com>
2026-05-11 15:11:09 +0100

    feat: update robots.txt rules to enhance SEO and restrict access
    - Expanded the robots.txt rules to include disallow directives for specific event and gallery-related paths, improving site indexing and SEO management.
    - Maintained existing allow rule for general access while ensuring sensitive or unnecessary paths are excluded from search engine indexing.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,7 +3,28 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   const base = process.env.NEXT_PUBLIC_SITE_URL || 'https://yadahworld.com'
   return {
-    rules: { userAgent: '*', allow: '/' },
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/events/*?*ical=1',
+        '/events/*?*outlook-ical=1',
+        '/events/list/',
+        '/events/month/',
+        '/event-category/',
+        '/*?tribe-bar-date*',
+        '/*?tribe_venue*',
+        '/*?__wpdm*',
+        '/release-category*',
+        '/releases/page/',
+        '/gallery-category*',
+        '/download/',
+        '/discography/',
+        '/embed/',
+        '/yalic/',
+        '/legal/',
+      ],
+    },
     sitemap: `${base}/sitemap.xml`,
   }
 }
